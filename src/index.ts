0daae2f22c35ca7fa986ba991705bae365ac57bd
Felipe Cotrin <felipe.cotrin@hotmail.com>
2022-10-04 13:40:43 -0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import axios from "axios"
+
+type Mission = {
+    name: string;
+    flight: string
+}
+
+type Ship = {
+    active: boolean
+    missions: Mission[]
+    name: string
+}
+
+interface SpaceXResponse {
+    data: {
+        ships: Ship[]
+    }
+}
+
+async function main() {
+    const url: string = 'https://api.spacex.land/graphql'
+    const query: string = `{
+      ships {
+        active
+        name
+        missions {
+          name
+          flight
+        }
+      }
+    }`
+
+    try {
+        console.log('Fetching data from SpaceX...')
+        const { data: graphqlResponse } = await axios.post<SpaceXResponse>(
+            url,
+            { query }
+        )
+
+        const { ships } = graphqlResponse.data
+
+        // Status,Ship-Name,Mission (?) Premissa que a ultima missão do array é a missão atual
+        let csv = 'Status,Ship-Name,Mission'
+
+        console.log('Writing data into CSV...')
+        for (const ship of ships) {
+            if (ship.active) {
+                const shipMission = ship.missions.at(-1)?.name
+
+                csv += `\n${ship.active},${ship.name},${shipMission}`
+            }
+        }
+
+        console.log('Data processed successfully...')
+        
+        // Console Data
+        console.table(csv)
+
+        // Write CSV File manually
+        // require("fs").writeFileSync("test.csv", csv)
+
+    } catch (error) {
+        console.error(`Error fetching data from ${url}`)
+    }
+
+
+}
+main()
